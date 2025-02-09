42a647653ec951fa6c62e266fc3638ca3be7425c
amossohotra0 <amossohotra0@gmail.com>
2025-02-09 10:00:00 -0500

    Footer Component

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,8 @@
+// src/components/layout/Footer.jsx
+const Footer = () => (
+  <footer className="bg-orange-400 text-white p-4 text-center">
+    <p>&copy; 2025 MyToolbox. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
